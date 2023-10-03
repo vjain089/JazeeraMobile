327c9c29013d370d47d1b7fd685a7d3f679cdace
unknown <juancmouhape@gmail.com>
2023-10-03 15:06:31 -0300

    Se sube TestSuite

---
+++ Test Suites/Prueba.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Prueba</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>1a17c905-1b19-46c7-9860-2f7c3311b7c1</testSuiteGuid>
+   <testCaseLink>
+      <guid>ea49a7e8-878e-4eff-9f0a-c87ee3bb9e19</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PruebaMobileTestCase</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
