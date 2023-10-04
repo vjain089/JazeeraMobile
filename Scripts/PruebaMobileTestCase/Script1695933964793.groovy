7ec20eb869597720cbc57c0dec4d4b5f8a414e88
lopezrichard <84924555+lopezrichard@users.noreply.github.com>
2023-10-04 15:09:42 +0100

    Update Script1695933964793.groovy

--- Scripts/PruebaMobileTestCase/Script1695933964793.groovy
+++ Scripts/PruebaMobileTestCase/Script1695933964793.groovy
@@ -3,22 +3,25 @@ import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.configuration.RunConfiguration
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
 import com.kms.katalon.core.testcase.TestCase as TestCase
 import com.kms.katalon.core.testdata.TestData as TestData
 import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.util.internal.PathUtil
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
-def apkPath = System.getProperty("user.dir") + '\\General-Store.apk'
-println("Ruta del archivo APK: " + apkPath)
-Mobile.startApplication(apkPath, true)
+import org.openqa.selenium.Keys
+import org.xbill.DNS.APLRecord as Keys
+
+def appPath = PathUtil.relativeToAbsolutePath("apkTest.apk", RunConfiguration.getProjectDir())
+Mobile.startApplication(appPath, true)
 
 Mobile.setText(findTestObject('Object Repository/PruebaMobileObject/android.widget.EditText - Enter name here'), 'Juan Cruz', 
     0)
